4702243d7a6d3dfc50e329aca05b400adceae92f
Jon Borrelli <borrejj@gmail.com>
2015-07-20 17:18:20 -0400

    start fixing functional response issues

--- Rscripts/MODELtest.R
+++ Rscripts/MODELtest.R
@@ -1,5 +1,10 @@
-
-
+LV.fr <- function(N, A, h){
+  aN <- A
+  aN[A < 0] <-(A * as.vector(N))[A < 0]
+  aN[A > 0] <- (t(A) * as.vector(N))[t(A) > 0]
+  return(t(sapply(1:nrow(A), function(x){aN[x,]/(1 + h * rowSums(abs(aN)))[x]})))
+}
+# something still wrong -- doesnt add up
 
 A <- matrix(c(0,1,0,-1,0,1,0,-1,0), nrow = 3) * matrix(runif(9, 0, 1), nrow = 3)
 #A <- matrix(c(0,1,1,-1,0,0,-1,0,0), nrow = 3) * matrix(runif(9, 0, 1), nrow = 3)
@@ -9,9 +14,9 @@ t = 50
 
 N <- c(100, 15, 20)
 
-Nt <- matrix(nrow = t, ncol = nrow(A))
-for(i in 1:t){
-  N <- N + c(1, 1, 1) * N * LV.fr(N, A, h) - (.2 * N) + (c(1,0,0) * N * (1 - (N/K))) 
+Nt <- matrix(N, nrow = t+1, ncol = nrow(A), byrow = T)
+for(i in 2:(t+1)){
+  N <- N + c(1, .1, .1) * LV.fr(N, A, h) %*% N - (.2 * N) + (c(1,0,0) * N * (1 - (N/K))) 
   N[N<1] <- 0
   Nt[i,] <- N
 }
@@ -39,6 +44,24 @@ lvmodel<-function(t,state,parameters){
 
 times<-seq(0,50,by=1)
 
-out<-ode(y=state,times=times,func=lvmodel,parms=parameters)
+out<-ode(y=state,times=times,func=lvmodel,parms=parameters, method = "adams")
 
-matplot(out[,2:4])
+matplot(out[,2:4], typ = "l")
+
+
+parameters<-c(r=1,K=K)
+
+state<-c(N=100)
+
+ss.test <- function(t,state,parameters){
+  with(as.list(c(state,parameters)),{
+    #rate of change
+    dN<-r*N*(1-N/K)
+    #return rate of change
+    list(c(dN))
+  })
+}
+
+times<-seq(1,20,by=1)
+
+out<-ode(y=state,times=times,func=ss.test,parms=parameters)
